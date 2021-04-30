626783121b950689dad0f872ef48edbf416b2eea
anthfgreco <popthesmart@hotmail.com>
2021-04-30 16:04:25 -0400

    Time: 116 ms (76.20%), Space: 21.5 MB (20.10%) - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -1,17 +1,16 @@
-"""
-O(n) time complexity, one pass
-O(n) space, dictionary holds list of all elements
-
-1.  Algorithm iterates over all items in the list and adds them one-by-one
-    to dictionary.
-2.  If item is already in dictionary, return True because there's a duplicate
-3.  If all items were looped through, return False because there's no duplicates
-
-Takes advantage of dictionary lookup being O(1)
-"""
-
 class Solution:
   def containsDuplicate(self, nums: List[int]) -> bool:
+    """
+    O(n) time complexity, one pass
+    O(n) space, dictionary holds list of all elements
+
+    1.  Algorithm iterates over all items in the list and adds them one-by-one
+        to dictionary.
+    2.  If item is already in dictionary, return True because there's a duplicate
+    3.  If all items were looped through, return False because there's no duplicates
+
+    Takes advantage of dictionary lookup being O(1)
+    """
     dict = {}
     for n in nums:
       if n in dict:
