0ff84b4ee7e6ee59a4b44eb59ed21f7769b5a68f
popTheSmart <popthesmart@hotmail.com>
2021-04-30 18:06:50 -0400

    update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -89,14 +89,39 @@ class Solution:
     
     return maxProfit
 
+  def maxSubArrayBruteForce(self, nums: List[int]) -> int:
+    """
+    Bruteforce solution, O(n^2) time complexity
+
+    Tries every permutation of subarrays
+    """
+    maxSum = 0
+
+    # Get sum of all subarray permutations
+    for i in range(len(nums)):
+      for j in range(i, len(nums)):
+        tmpsum = sum(nums[i:j])
+        if tmpsum > maxSum:
+          maxSum = tmpsum
+    
+    return maxSum
+
+  def maxSubArray(self, nums: List[int]) -> int:
+    """
+    O(n) time complexity
+    Kadane's Algorithm
+    """
+    for i in range(1, len(nums)):
+      if nums[i-1] > 0:
+        nums[i] += nums[i-1]
+   
+    return max(nums)
+
+
 
 
 
 
 
 sol = Solution()
-#print(  sol.containsDuplicate([1,2,3,4,5,6,2])     )
-#print(  sol.twoSum([2,7,11,15], 9)                 )
-#print(  sol.twoSum([3,2,4], 6)                     )
-print(  sol.maxProfit([7,1,5,3,6,4])    )
-
+print(sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
