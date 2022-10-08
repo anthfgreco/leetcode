ff936ff0dec129cb8381014f8a3fd9621cf0f7f2
anthfgreco <anthony1.greco@ryerson.ca>
2022-10-07 20:10:32 -0400

    Time: 46 ms (99.99%), Space: 15.5 MB (9.02%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,26 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        """
-        [2,7,11,15], target = 9
-        list - target
-        dictionary = [-7, -5, 2, 6]
-        At i=1 (7) we want to search the dictionary for -7 which will return the correct answer.
-        
-        [3,2,4], target = 6
-        list - target
-        dictionary = [-3,-4,-2]
-        At i=1 (7) we want to search the dictionary for -7 which will return the correct answer.
-        
-        Subtract target from nums
-        Create dictionary where nums-target -> index
-        Search dictionary for num*-1, if found, return both indices
-        """
+        # nums = [2,7,11,15], target = 9
+        # nums - target = [-7, -2, 2, 6]
         d = {}
-
         for i, num in enumerate(nums):
-            # Covers [X, X] case
-            if num-target not in d:
-                d[num-target] = i
-            if num*-1 in d and i != d[num*-1]:
-                return sorted([i, d[num*-1]])
-        
+            if -1*num in d:
+                return [d[-1*num], i]
+            d[num - target] = i
+            
